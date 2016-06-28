7dc5854fa714475d2f8b1aebe0b33c569fd32999
Tristan Timblin <tatimblin@gmail.com>
2016-06-28 14:48:44 -0400

    Week 02 changes
    Starting to build solo pong game - mouse movement and canvas size/color.

--- js/game.js
+++ js/game.js
@@ -18,5 +18,40 @@ window.cancelRequestAnimFrame = ( function() {
 			window.msCancelRequestAnimationFrame     ||
 		clearTimeout;
 } )();
+// Do No Touch Above Code
 
-console.log('Holla');
+
+
+// ### Step 01 ..tat.. Create game canvas and track mouse position ###
+
+var gameCanvas = document.getElementById("canvas");
+// Store HTML5 canvas tag into JS variable
+
+var ctx = gameCanvas.getContext("2d"); // Create context 2D
+var W = window.innerWidth;
+var H = window.innerHeight;
+
+var mouseObj = {};
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas() {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, W, H);
+}
+paintCanvas();
+
+
+function trackPosition(evt) {
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("Cursor x is: " + mouseObj.x + " Cursor y is: " + mouseObj.y);
+}
+gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+
+
+
+
+
