f4b9a484e6b5920b5d2f173669601033d9347c37
Tristan Timblin <tatimblin@gmail.com>
2016-07-05 14:33:28 -0400

    Update main screen
    Need main screen objects.

--- js/game.js
+++ js/game.js
@@ -36,6 +36,8 @@ var mouseObj = {};
 gameCanvas.width = W;
 gameCanvas.height = H;
 
+
+// Step 02 ..tat.. Clear page canvas by covering it in black
 function paintCanvas() {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, W, H);
@@ -49,6 +51,60 @@ function trackPosition(evt) {
     console.log("Cursor x is: " + mouseObj.x + " Cursor y is: " + mouseObj.y);
 }
 gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+
+// Step 03 ..tat.. Place a ball on the canvas
+var ball = {}; // Ball Object
+ball = {
+    x: 50,
+    y: 50,
+    r: 15,
+    c: "#ffffff",
+    vx: 4,
+    vy: 8,
+    
+    draw : function() {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
+}
+ball.draw();
+
+// Step 04 ..tat.. Place a start button on the canvas
+var startBtn = {}; //Start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+    
+    draw: function() {
+        ctx.strokeStyle = "#ffffff";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        
+        ctx.font = "18px Arial, sans-serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText("Start", W / 2, H / 2);
+    }
+}
+startBtn.draw();
+
+// Step 05 ..tat.. Place score and points on canvas
+var points = 0; // Game Points
+function paintScore() {
+    ctx.fillStyle = '#FFFFFF';
+    ctx.font = "18px Arial, sans-serif";
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Score: ' + points, 20, 20);
+}
+paintScore();
+
 
 
 
